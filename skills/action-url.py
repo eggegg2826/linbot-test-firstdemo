793f2d83ec2776156778b8341c25596734b3f577
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-08 23:49:25 +0800

    Update action-url.py

--- skills/action-url.py
+++ skills/action-url.py
@@ -12,10 +12,10 @@ def get(message_request: MessageRequest):
         alt_text='Actions',
         template=ButtonsTemplate(
             title='111年數位共好規範',
-            text='點擊查看',
+            text='目前112年數位共好規範尚未公佈，詳細規範請以112年數位共好規範為主',
             actions=[
                 URIAction(
-                    label='111規範',
+                    label='點擊查看',
                     uri='https://www.moeasmea.gov.tw/files/7853/926E142A-7BF5-40DD-9080-534B8B8690CE'
                 )
             ]
