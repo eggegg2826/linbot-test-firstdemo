c612a213b971786a88a5d702e499af7c2e3c2b8d
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-08 17:53:31 +0800

    Update message.py

--- skills/message.py
+++ skills/message.py
@@ -2,7 +2,7 @@ from linebot.models import TextSendMessage
 from models.message_request import MessageRequest
 from skills import add_skill
 
-@add_skill('規範')
+@add_skill('{text}')
 def get(message_request: MessageRequest):
     msg = TextSendMessage(text='今年的規範還沒出來')
     return [
