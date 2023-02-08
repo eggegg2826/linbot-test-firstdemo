8fce3bceb68fcc3d619c0fd71f9e4b918cc8f596
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-08 17:45:49 +0800

    Update message.py

--- skills/message.py
+++ skills/message.py
@@ -1,24 +1,10 @@
-msg = TextSendMessage(text='Hello')
-return [msg]
-```
+from linebot.models import TextSendMessage
+from models.message_request import MessageRequest
+from skills import add_skill
 
-# 回傳LINE emoji
-
-```python
-
-txt = '$ LINE emoji $'
-emoji = [
-    {
-        "index": 0,
-        "productId": "5ac1bfd5040ab15980c9b435",
-        "emojiId": "001"
-    },
-    {
-        "index": 13,
-        "productId": "5ac1bfd5040ab15980c9b435",
-        "emojiId": "002"
-    }
-]
-# print(txt.find('$',2))
-
-emoji_message = TextSendMessage(text='$ LINE emoji $', emojis=emoji)
+@add_skill('規範')
+def get(message_request: MessageRequest):
+    msg = TextSendMessage(text='今年的規範還沒出來')
+    return [
+        msg
+    ]
