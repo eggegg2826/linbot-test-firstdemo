c4041b2f1ca1cf520b2ad603a8ade681e858ea5a
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-08 23:05:50 +0800

    Update and rename message.py to actionURL.py

---
+++ skills/actionURL.py
@@ -0,0 +1,23 @@
+from linebot.models import TextSendMessage
+from linebot.models.temlate import ButtonsTemplate
+from linebot.models.actions import URIAction
+from models.message request import MessageRequest
+from skills import add_skill
+
+
+@add_skill('規範')
+def get(message_request: MessageRequest):
+
+    msg = TemplateSendMessage(
+    alt_text='Actions',
+    template=ButtonsTemplate(
+        title='Menu',
+        text='Please Click',
+        actions=[
+            URIAction(
+                label='111規範',
+                uri='https://www.moeasmea.gov.tw/files/7853/926E142A-7BF5-40DD-9080-534B8B8690CE'
+                )
+            ]
+        )
+    )
