1a7a8173a1ec96b9aa7b5dce94364de2540adc9c
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-09 17:22:50 +0800

    Create action-sign_up.py

---
+++ skills/action-sign_up.py
@@ -0,0 +1,27 @@
+from linebot.models import TemplateSendMessage
+from linebot.models.template import ButtonsTemplate
+from linebot.models.actions import URIAction
+from models.message_request import MessageRequest
+from skills import add_skill
+
+
+@add_skill('報名')
+def get(message_request: MessageRequest):
+
+    msg = TemplateSendMessage(
+        alt_text='案例分享會報名',
+        template=ButtonsTemplate(
+            title='案例分享會報名',
+            text='台中場-活動時間:2/15(三)14：00,高雄場-活動時間：2/16(四)14：00,台北場-活動時間：2/21(二)14：00',
+            actions=[
+                URIAction(
+                    label='點擊報名',
+                    uri='https://forms.gle/jKriGfsCVJ7pdpW47?openExternalBrowser=1'
+                )
+            ]
+        )
+    )
+
+    return [
+        msg
+    ]
