e15a5298e9a171ead67a3b8c1af9a5b46bee1ca0
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-09 17:11:13 +0800

    Update action-Sign_up.py

--- skills/action-Sign_up.py
+++ skills/action-Sign_up.py
@@ -9,13 +9,13 @@ from skills import add_skill
 def get(message_request: MessageRequest):
 
     msg = TemplateSendMessage(
-        alt_text='Signup',
+        alt_text='Actions',
         template=ButtonsTemplate(
             title='案例分享會報名',
-            text='台中場-活動時間：2/15(三) 14：00
-                  高雄場-活動時間：2/16(四) 14：00
-                  台北場-活動時間：2/21(二) 14：00',
-            Signup=[
+            text='台中場-活動時間：2/15(三)14：00
+                  高雄場-活動時間：2/16(四)14：00
+                  台北場-活動時間：2/21(二)14：00',
+            actions=[
                 URIAction(
                     label='點擊報名',
                     uri='https://forms.gle/jKriGfsCVJ7pdpW47?openExternalBrowser=1'
