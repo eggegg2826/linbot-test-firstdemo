a092667230b21dc8112b80e949b8f97d76f88336
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-09 17:55:43 +0800

    Create action_url.py

---
+++ skills/action_url.py
@@ -0,0 +1,27 @@
+from linebot.models import TemplateSendMessage
+from linebot.models.template import ButtonsTemplate
+from linebot.models.actions import URIAction
+from models.message_request import MessageRequest
+from skills import add_skill
+
+
+@add_skill('規範')
+def get(message_request: MessageRequest):
+
+    msg = TemplateSendMessage(
+        alt_text='111數位共好規範',
+        template=ButtonsTemplate(
+            title='111數位共好規範',
+            text='目前112年數位共好規範尚未公佈，詳細規範請以112年數位共好規範為主。',
+            actions=[
+                URIAction(
+                    label='點擊查看規範',
+                    uri='https://www.moeasmea.gov.tw/files/7853/926E142A-7BF5-40DD-9080-534B8B8690CE?openExternalBrowser=1'
+                )
+            ]
+        )
+    )
+
+    return [
+        msg
+    ]
