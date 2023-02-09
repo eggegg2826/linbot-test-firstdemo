ea6134c3484141463793dd6e435024d7ac75c10d
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-09 17:47:54 +0800

    Update action-sign_up.py

--- skills/action-sign_up.py
+++ skills/action-sign_up.py
@@ -22,6 +22,23 @@ def get(message_request: MessageRequest):
         )
     )
 
+@add_skill('規範')
+def get(message_request: MessageRequest):
+
+    msg1 = TemplateSendMessage(
+        alt_text='111數位共好規範',
+        template=ButtonsTemplate(
+            title='111數位共好規範',
+            text='目前112年數位共好規範尚未公佈，詳細規範請以112年數位共好規範為主。',
+            actions=[
+                URIAction(
+                    label='點擊查看規範',
+                    uri='https://www.moeasmea.gov.tw/files/7853/926E142A-7BF5-40DD-9080-534B8B8690CE?openExternalBrowser=1'
+                )
+            ]
+        )
+    )
+
     return [
-        msg
+        msg,msg1
     ]
