00e9f554a36997715ef9ae51256b7bc2791f6083
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-10 14:39:12 +0800

    Update imafe.py

--- skills/imafe.py
+++ skills/imafe.py
@@ -1,14 +1,23 @@
 from linebot.models import ImageSendMessage
 from models.message_request import MessageRequest
 from skills import add_skill
+import random
 
-@add_skill('抽別人')
+@add_skill('抽水果')
 def get(message_request: MessageRequest):
 
-    p1  = ImageSendMessage(
-          original_content_url='https://via.placeholder.com/1024x768/333.png/fff',
-          preview_image_url='https://via.placeholder.com/800x600/333.png/fff')
+    # 處理字串
+    p1 = ImageSendMessage(original_content_url='https://via.placeholder.com/1024x768/333.png/fff',
+    preview_image_url='https://via.placeholder.com/800x600/333.png/fff')
+    p2 = ImageSendMessage(original_content_url='https://i.imgur.com/nlxEVPp.jpg',
+    preview_image_url='https://i.imgur.com/nlxEVPp.jpg')
+        image = (p1,p2)
+            print(image)
+
+    # 隨機挑選一個項目
+    result = random.choice(image)
+        print(result)
 
     return [
-        p1
+        result
     ]
