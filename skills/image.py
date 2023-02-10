dd1b5645749f3ec2e961cea866e0fbea2b537bcf
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-10 11:05:50 +0800

    Update image.py

--- skills/image.py
+++ skills/image.py
@@ -7,9 +7,9 @@ def get(message_request: MessageRequest):
 
     msg = ImageSendMessage(
         #點開後的圖片
-        original_content_url='https://drive.google.com/file/d/1_7NGhE0ER9cmp4VdIiAfsGrVziqx3DGm/view?usp=share_link',
+        original_content_url='https://i.imgur.com/nlxEVPp.jpg',
         #預覽圖
-        preview_image_url='https://drive.google.com/file/d/1_7NGhE0ER9cmp4VdIiAfsGrVziqx3DGm/view?usp=share_link')
+        preview_image_url='https://i.imgur.com/nlxEVPp.jpg')
 
     return [
         msg
