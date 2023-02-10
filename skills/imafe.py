9e1dca31164987996f17cdc4026d2d8b70668302
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-10 14:42:28 +0800

    Update imafe.py

--- skills/imafe.py
+++ skills/imafe.py
@@ -6,18 +6,11 @@ import random
 @add_skill('抽水果')
 def get(message_request: MessageRequest):
 
-    # 處理字串
     p1 = ImageSendMessage(original_content_url='https://via.placeholder.com/1024x768/333.png/fff',
     preview_image_url='https://via.placeholder.com/800x600/333.png/fff')
     p2 = ImageSendMessage(original_content_url='https://i.imgur.com/nlxEVPp.jpg',
     preview_image_url='https://i.imgur.com/nlxEVPp.jpg')
-        image = (p1,p2)
-            print(image)
-
-    # 隨機挑選一個項目
-    result = random.choice(image)
-        print(result)
 
     return [
-        result
+        p1,p2
     ]
