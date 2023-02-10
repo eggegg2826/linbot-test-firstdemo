822bacbe31100e158dd49ef23a645ed790507754
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-10 11:55:36 +0800

    Update message.py

--- skills/message.py
+++ skills/message.py
@@ -7,10 +7,8 @@ import random
 @add_skill('抽黃皓的朋友')
 def get(message_request: MessageRequest):
 
-    # /吃什麼 烤雞.炸雞.鹹酥雞
-
     # 處理字串
-    foods_str = message_request.message.replace("黃皓的朋友 ", "")
+    foods_str = message_request.message.replace("抽黃皓的朋友 ", "")
     foods = foods_str.split('.')
     print(foods)
 
