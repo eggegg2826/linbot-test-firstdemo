f6537fe913e44bf6b03f2b858a76fe6c99b617af
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-10 14:25:33 +0800

    Update imafe.py

--- skills/imafe.py
+++ skills/imafe.py
@@ -5,11 +5,9 @@ from skills import add_skill
 @add_skill('抽自己')
 def get(message_request: MessageRequest):
 
-    msg = p1
-    
-    p1 = ImageSendMessage(
-         original_content_url='https://via.placeholder.com/1024x768/333.png/fff',
-         preview_image_url='https://via.placeholder.com/800x600/333.png/fff')
+    msg = ImageSendMessage(
+          original_content_url='https://via.placeholder.com/1024x768/333.png/fff',
+          preview_image_url='https://via.placeholder.com/800x600/333.png/fff')
 
     return [
         msg
