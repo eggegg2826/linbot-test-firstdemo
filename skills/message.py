3670e9ad4d1ccc12c2fa74c18f4565b0fb367192
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-10 11:47:58 +0800

    Update message.py

--- skills/message.py
+++ skills/message.py
@@ -1,10 +1,26 @@
-from linebot.models import TextSendMessage
+from linebot.models import ImageSendMessage
 from models.message_request import MessageRequest
 from skills import add_skill
+import random
 
-@add_skill('黃皓')
+
+@add_skill('抽黃皓的朋友')
 def get(message_request: MessageRequest):
-    msg = TextSendMessage('帥哥')
+
+    # /吃什麼 烤雞.炸雞.鹹酥雞
+
+    # 處理字串
+    foods_str = message_request.message.replace("黃皓的朋友 ", "")
+    foods = foods_str.split('.')
+    print(foods)
+
+    # 隨機挑選一個項目
+    result = random.choice(foods)
+    print(result)
+
+    # 處理回傳訊息
+    meg = ImageSendMessage(result)
+
     return [
-        msg
+        meg
     ]
