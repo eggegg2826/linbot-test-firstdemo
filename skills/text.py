e8902bcaec712f7ff99200351a65811a0f1a6bfb
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-13 01:20:48 +0800

    Update text.py

--- skills/text.py
+++ skills/text.py
@@ -3,7 +3,7 @@ from linebot.models import TextSendMessage
 from models.message_request import MessageRequest
 from skills import add_skill
 
-@add_skill('{not_match}')
+@add_skill('')
 def get(message_request: MessageRequest):
     msg = TextSendMessage(text="查詢")
     msg1 = TextSendMessage(text="查詢2")
