4af2aab0db5de177cae81698c91d6c8dc10a1568
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-13 01:31:37 +0800

    Update text.py

--- skills/text.py
+++ skills/text.py
@@ -7,4 +7,7 @@ from skills import add_skill
 def get(message_request: MessageRequest):
     msg = TextSendMessage(text="查詢")
     msg1 = TextSendMessage(text="查詢2")
-    return[msg,msg1]
+    if msg == TextSendMessage(text="查詢"):
+        return[msg]
+    else:
+        return[msg1]
