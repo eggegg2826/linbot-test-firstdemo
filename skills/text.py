b208ac8c9267c7d9e6dfc4167ee49276b4d61ee8
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-13 00:15:00 +0800

    Update text.py

--- skills/text.py
+++ skills/text.py
@@ -4,9 +4,6 @@ from skills import add_skill
 
 @add_skill('{message_request.message}')
 def get(message_request: MessageRequest):
-    msg = TextSendMessage("查詢")
-    msg1 = TextSendMessage("無此資料")
-    if msg in {message_request.message}:
-        return [msg]
-    else:
-        return [msg1]
+    return [
+        TextSendMessage(text=f'You said: {message_request.message}')
+    ]
