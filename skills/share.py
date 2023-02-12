a21029f39f8311da0e7916e61727dee5d0e3551d
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-13 02:19:46 +0800

    Create share.py

---
+++ skills/share.py
@@ -0,0 +1,27 @@
+from linebot.models import TemplateSendMessage
+from linebot.models.template import ButtonsTemplate
+from linebot.models.actions import URIAction
+from models.message_request import MessageRequest
+from skills import add_skill
+
+
+@add_skill('報名')
+def get(message_request: MessageRequest):
+
+    msg = TemplateSendMessage(
+        alt_text='案例分享會報名',
+        template=ButtonsTemplate(
+            title='案例分享會報名',
+            text='台中場-2/15(三)下午兩點\n高雄場-2/16(四)下午兩點\n台北場-2/21(二)下午兩點',
+            actions=[
+                URIAction(
+                    label='點擊報名',
+                    uri='https://forms.gle/jKriGfsCVJ7pdpW47?openExternalBrowser=1'
+                )
+            ]
+        )
+    )
+    
+    return [
+        msg
+    ]
