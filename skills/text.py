e828c804aa2672de7286dc21fdf4b5d29149dcc5
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-13 00:25:10 +0800

    Update text.py

--- skills/text.py
+++ skills/text.py
@@ -2,8 +2,9 @@ from linebot.models import TextSendMessage
 from models.message_request import MessageRequest
 from skills import add_skill
 
-@add_skill('{message_request.message}')
+@add_skill('{message_request}')
 def get(message_request: MessageRequest):
-    return [
-        TextSendMessage(text=f'You said: {message_request.message}')
-    ]
+    if "查詢" in TextSendMessage{message_request}:
+        return ["你好"]
+    else:
+        return ["錯誤"]
