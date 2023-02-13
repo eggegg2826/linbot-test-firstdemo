6cade65fb158f9d8de3a894738efb370324d1e0f
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-13 18:06:53 +0800

    Update input.py

--- skills/input.py
+++ skills/input.py
@@ -4,7 +4,7 @@ from skills import add_skill
 
 @add_skill('查詢')
 def get(message_request: MessageRequest):
-    msg = TextSendMessage("目前MENU功能如下:1.輸入『規範』查看最新規範,end='', 2.輸入『報名』報名案例分享會end=''")
+    msg = TextSendMessage("目前MENU功能如下:1.輸入『規範』查看最新規範2.輸入『報名』報名案例分享會")
     return [
         msg
     ]
