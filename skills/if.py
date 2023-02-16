0b351e9aabdc6a2ce377886628d53e7f17ad4362
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-16 21:46:45 +0800

    Update if.py

--- skills/if.py
+++ skills/if.py
@@ -1,10 +1,12 @@
+from typing import Text
 from linebot.models import TextSendMessage
 from models.message_request import MessageRequest
 from skills import add_skill
 
-@add_skill('查看 ')
+@add_skill('{not_match}')
 def get(message_request: MessageRequest):
-    if "規範" in {message_request.message}:
-        return[TextSendMessage(text="成功")]
+    qwe = {message_request.message}
+    if "凱哥" in qwe:
+        return[TextSendMessage(text="凱哥在")]
     else:
-        return[TextSendMessage(text="失敗")]
+        return[TextSendMessage(text=" ")]
