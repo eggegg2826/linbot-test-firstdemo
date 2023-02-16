fefbeddcc6f5335b8b164fcf7f8b9ea9a07a72a5
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-16 17:42:19 +0800

    Create if.py

---
+++ skills/if.py
@@ -0,0 +1,12 @@
+from typing import Text
+from linebot.models import TextSendMessage
+from models.message_request import MessageRequest
+from skills import add_skill
+
+@add_skill('好吃 ')
+def get(message_request: MessageRequest):
+    qwe = {message_request.message}
+    if "規範" in qwe:
+        return[TextSendMessage(text="成功")]
+    else:
+        return[TextSendMessage(text="失敗")]
