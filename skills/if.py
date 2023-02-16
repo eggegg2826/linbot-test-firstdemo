aa835bd1a2570353b6adaf98111efb18214e2c84
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-16 21:39:01 +0800

    Update if.py

--- skills/if.py
+++ skills/if.py
@@ -1,9 +1,8 @@
-from typing import Text
 from linebot.models import TextSendMessage
 from models.message_request import MessageRequest
 from skills import add_skill
 
-@add_skill('肚子餓')
+@add_skill('查看 ')
 def get(message_request: MessageRequest):
     qwe = {message_request.message}
     if "規範" in qwe:
