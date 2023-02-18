523fa03cda9999303a552468ae046c94e18d5ce9
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-18 17:31:00 +0800

    Update run_join_in.py

--- skills/run_join_in.py
+++ skills/run_join_in.py
@@ -4,7 +4,7 @@ from skills import add_skill
 
 @add_skill('成為軟協會員')
 def get(message_request: MessageRequest):
-    msg = TextSendMessage('此功能建置中.../n歡迎E-mail來信本協會:cisaims@cisanet.org.tw')
+    msg = TextSendMessage('此功能建置中...\n歡迎E-mail來信本協會:cisaims@cisanet.org.tw')
     return [
         msg
     ]
