c8a528fbd6289777b5db0a2fa72dd5947a494fce
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-18 15:01:21 +0800

    Update rule.py

--- skills/rule.py
+++ skills/rule.py
@@ -1,5 +1,5 @@
-from linebot.models import TextSendMessage
-from linebot.models.template import ButtonsTemplate, TemplateSendMessage
+from linebot.models import TextSendMessage, TemplateSendMessage
+from linebot.models.template import ButtonsTemplate
 from linebot.models.actions import URIAction
 from models.message_request import MessageRequest
 from skills import add_skill
@@ -39,6 +39,19 @@ def get(message_request: MessageRequest):
         )
     )
     
+    no = TemplateSendMessage(
+        alt_text='112年推動企業數位共好計畫提案說明會',
+        template=ButtonsTemplate(
+            title='112年推動企業數位共好計畫提案說明會',
+            text='台中場-2/15(三)下午兩點\n高雄場-2/16(四)下午兩點\n台北場-2/21(二)下午兩點',
+            actions=[
+                URIAction(
+                    label='報名說明會',
+                    uri='https://forms.gle/jKriGfsCVJ7pdpW47?openExternalBrowser=1'
+                )
+            ]
+        )
+    )
     #IF規則
     if "規範" in mg_st:
         return[rule]
