69cd45219b295b3b145f1cff52114c3ce15f612c
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-18 17:01:55 +0800

    Create run_qa.py

---
+++ skills/run_qa.py
@@ -0,0 +1,10 @@
+from linebot.models import TextSendMessage
+from models.message_request import MessageRequest
+from skills import add_skill
+
+@add_skill('計畫常見Q&A')
+def get(message_request: MessageRequest):
+    msg = TextSendMessage('此功能建置中.../n若有相關問題，歡迎E-mail來信本協會:cisaims@cisanet.org.tw')
+    return [
+        msg
+    ]
