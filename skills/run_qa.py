6b145064d563815196950ad21be202d700e416ee
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-19 15:56:28 +0800

    Update run_qa.py

--- skills/run_qa.py
+++ skills/run_qa.py
@@ -4,7 +4,7 @@ from skills import add_skill
 
 @add_skill('計畫常見Q&A')
 def get(message_request: MessageRequest):
-    msg = TextSendMessage('此功能建置中...\n若有相關問題，歡迎E-mail來信本協會:cisaims@cisanet.org.tw')
+    msg = TextSendMessage('此功能建置中...\n若有相關問題，歡迎E-mail來信:cisaims@cisanet.org.tw')
     return [
         msg
     ]
