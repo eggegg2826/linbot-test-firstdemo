a7b896ad10ced65bc020fdaddf5fe89c383c0270
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-20 18:51:13 +0800

    Update run_join_in.py

--- skills/run_join_in.py
+++ skills/run_join_in.py
@@ -1,27 +1,11 @@
-from linebot.models import TemplateSendMessage
-from linebot.models.template import ButtonsTemplate
-from linebot.models.actions import URIAction
+from linebot.models import TextSendMessage
 from models.message_request import MessageRequest
 from skills import add_skill
 
-
 @add_skill('成為軟協會員')
 def get(message_request: MessageRequest):
-
-    msg = TemplateSendMessage(
-        alt_text='成為軟協會員',
-        template=ButtonsTemplate(
-            title='成為軟協會員',
-            text='竭誠歡迎您成為軟協會員，若收到您填寫的表單後，我們將盡快與您聯絡。',
-            actions=[
-                URIAction(
-                    label='加入成為軟協會員',
-                    uri='https://forms.gle/5341yP5YqHsorWcC7'
-                )
-            ]
-        )
-    )
-    
+    msg = TextSendMessage('此功能建置中...\n歡迎E-mail來信本協會:cisaims@cisanet.org.tw')
     return [
         msg
     ]
+
