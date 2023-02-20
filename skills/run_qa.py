45c0a7e0d9dfc59ff5fcbea71806421b54666aeb
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-20 18:49:46 +0800

    Update run_qa.py

--- skills/run_qa.py
+++ skills/run_qa.py
@@ -1,27 +1,10 @@
-from linebot.models import TemplateSendMessage
-from linebot.models.template import ButtonsTemplate
-from linebot.models.actions import URIAction
+from linebot.models import TextSendMessage
 from models.message_request import MessageRequest
 from skills import add_skill
 
-
 @add_skill('計畫常見Q&A')
 def get(message_request: MessageRequest):
-
-    msg = TemplateSendMessage(
-        alt_text='計畫常見Q&A',
-        template=ButtonsTemplate(
-            title='計畫常見Q&A',
-            text='列出幾項關於數位共好計畫常見的問題，若還有疑問，請於服務時間來電。',
-            actions=[
-                URIAction(
-                    label='前往Q&A',
-                    uri='https://liff.line.me/1657887101-1DxD04M9'
-                )
-            ]
-        )
-    )
-    
+    msg = TextSendMessage('此功能建置中...\n若有相關問題，歡迎E-mail來信本協會:cisaims@cisanet.org.tw')
     return [
         msg
     ]
