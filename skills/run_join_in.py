a66eabd91b8811514b7a5db29f6df1b134893298
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-22 15:19:15 +0800

    Update run_join_in.py

--- skills/run_join_in.py
+++ skills/run_join_in.py
@@ -8,20 +8,20 @@ from skills import add_skill
 @add_skill('成為軟協會員')
 def get(message_request: MessageRequest):
 
-    msg = TemplateSendMessage(
-        alt_text='成為軟協會員',
-        template=ButtonsTemplate(
-            title='成為軟協會員',
-            text='竭誠歡迎您成為軟協會員，若收到您填寫的表單後，我們將盡快與您聯絡。',
+    buttons_template_message = TemplateSendMessage(
+    alt_text='成為軟協會員',
+    template=ButtonsTemplate(
+            thumbnail_image_url='https://imgur.com/a/IAaVOzR',
+            title='竭誠歡迎您成為軟協會員',
+            text='請填寫表單，收到您填寫的表單後，我們將會盡快與您聯絡。',
             actions=[
-                URIAction(
-                    label='加入成為軟協會員',
-                    uri='https://forms.gle/5341yP5YqHsorWcC7'
-                )
+                    URIAction(
+                            label='加入成為軟協會員',
+                            uri='https://forms.gle/5341yP5YqHsorWcC7'
+                    )
             ]
-        )
     )
-    
+    )
     return [
-        msg
+        buttons_template_message
     ]
