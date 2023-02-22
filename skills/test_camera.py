47164419d0f05b2f6cf134c0bca12e93e192c222
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-02-22 12:01:19 +0800

    Create test_camera.py

---
+++ skills/test_camera.py
@@ -0,0 +1,26 @@
+from linebot.models import TemplateSendMessage
+from linebot.models.template import ButtonsTemplate
+from linebot.models.actions import CameraAction, CameraRollAction
+from models.message_request import MessageRequest
+from skills import add_skill
+
+
+@add_skill('拍照')
+def get(message_request: MessageRequest):
+
+
+    camara = TemplateSendMessage(
+        alt_text='Actions',
+        template=ButtonsTemplate(
+            title='Menu',
+            text='相片選擇器',
+            actions=[
+                CameraAction(label='開啟相機'),
+                CameraRollAction(label='開啟相簿')
+            ]
+        )
+    )
+
+return [
+    camara
+]
