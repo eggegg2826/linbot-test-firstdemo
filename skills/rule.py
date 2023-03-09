71657dbc712f3132ffc303789c1d4fbaf0fc52e5
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-03-10 01:41:00 +0800

    Update rule.py

--- skills/rule.py
+++ skills/rule.py
@@ -67,7 +67,7 @@ def get(message_request: MessageRequest):
     )
     
     #IF規則
-    if ("規範" in mg_st) or ("計畫" in mg_st) or ("問題" in mg_st):
+    if ("規範" in mg_st) or ("計畫" in mg_st) or ("問題" in mg_st) or ("資訊" in mg_st):
         return[rule]
     elif ("A" in mg_st) or ("" in mg_st):
         return[share]
