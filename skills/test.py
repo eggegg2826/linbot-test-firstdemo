6d8559c361a05e4d83ab1faaec146abcdb8d2d9e
eggegg2826 <73411724+eggegg2826@users.noreply.github.com>
2023-03-15 13:41:27 +0800

    Update test.py

--- skills/test.py
+++ skills/test.py
@@ -6,16 +6,16 @@ app = FastAPI()
 
 @app.get("/scrape")
 async def scrape():
-    # 下載 Excel 檔案
+    # 下載 Excel檔案
     url = "https://docs.google.com/spreadsheets/d/1xT20il2JtGCHwjnB4Azbsfk5Xbi3_7fTrJKkNcI2Bko/edit?usp=sharing"
     response = requests.get(url)
     response.raise_for_status()
 
-    # 讀取 Excel 檔案
+    # 讀取 Excel檔案
     wb = openpyxl.load_workbook(filename=response.content)
     ws = wb.active
 
-    # 解析 Excel 檔案
+    # 解析 Excel檔案
     data = []
     for row in ws.iter_rows(min_row=2, values_only=True):
         name, age, address = row
